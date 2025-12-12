e4ba632d7537b36530283ced2aba6d1010e642b0
Ernesto Blejer <ernesto.blejer@gmail.com>
2025-12-12 14:49:24 -0300

    Agregado métodos post-put-delete

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -12,3 +12,41 @@ export const getProductById = async (req, res) => {
   }
   res.json(product);
 };
+
+export const createProduct = async (req, res) => {
+  if (typeof req.body.name == undefined) {
+    return res.status(422).json({ error: "El nombre es obligatorio" });
+  }
+
+  const { name, price, categories } = req.body;
+
+  const product = await model.createProduct({ name, price, categories });
+
+  res.status(201).json(product);
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, price, categories } = req.body;
+
+  const updatedProduct = await model.updateProduct(id, { name, price, categories });
+  
+  if (!updatedProduct) {
+    return res.status(404).json({ error: "Not Found" });
+  }
+  
+  res.json(updatedProduct);
+};
+
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  const deleted = await model.deleteProduct(id);
+
+  if (!deleted) {
+    return res.status(404).json({ error: "Not Found" });
+  }
+
+  res.json({ message: "Product deleted" });
+};
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -5,9 +5,15 @@ const router = Router();
 import {
   getAllProducts,
   getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
   } from "../controllers/products.controller.js";
 
 router.get("/products", getAllProducts);
 router.get("/products/:id", getProductById);
+router.post("/products", createProduct);
+router.put("/products/:id", updateProduct);
+router.delete("/products/:id", deleteProduct);
 
 export default router;
